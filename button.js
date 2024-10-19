f65b1d02d2a4f59be7c852b620f23837ec5cb453
Arrrz <aristria91@gmail.com>
2024-10-20 00:54:08 +0700

    Add files via upload

---
+++ button.js
@@ -0,0 +1,45 @@
+
+function togglesolid() {
+    var text = document.getElementById("SOLID");
+    if (text.style.display === "none") {
+        text.style.display = "block"; // Menampilkan teks
+    } else {
+        text.style.display = "none"; // Menyembunyikan teks
+    }
+}
+
+function toggleaktif() {
+    var text = document.getElementById("AKTIF");
+    if (text.style.display === "none") {
+        text.style.display = "block"; // Menampilkan teks
+    } else {
+        text.style.display = "none"; // Menyembunyikan teks
+    }
+}
+
+function togglekreatif() {
+    var text = document.getElementById("KREATIF");
+    if (text.style.display === "none") {
+        text.style.display = "block"; // Menampilkan teks
+    } else {
+        text.style.display = "none"; // Menyembunyikan teks
+    }
+}
+
+function toggletransformatif() {
+    var text = document.getElementById("TRANSFORMATIF");
+    if (text.style.display === "none") {
+        text.style.display = "block"; // Menampilkan teks
+    } else {
+        text.style.display = "none"; // Menyembunyikan teks
+    }
+}
+
+function toggleislami() {
+    var text = document.getElementById("ISLAMI");
+    if (text.style.display === "none") {
+        text.style.display = "block"; // Menampilkan teks
+    } else {
+        text.style.display = "none"; // Menyembunyikan teks
+    }
+}
