52cdf725708189fe5378a473948fb22fcb17d551
Arrrz <aristria91@gmail.com>
2024-10-20 14:39:23 +0700

    Add files via upload

--- button.js
+++ button.js
@@ -1,45 +1,65 @@
 
 function togglesolid() {
+    const button = document.getElementById("an");
     var text = document.getElementById("SOLID");
     if (text.style.display === "none") {
-        text.style.display = "block"; // Menampilkan teks
+        text.style.display = "block";
+        button.textContent = "Sembunyikan Teks";
     } else {
-        text.style.display = "none"; // Menyembunyikan teks
+        text.style.display = "none"; 
+        button.textContent = "Tampilkan";
+        
     }
 }
 
 function toggleaktif() {
+    const button = document.getElementById("bn");
     var text = document.getElementById("AKTIF");
     if (text.style.display === "none") {
-        text.style.display = "block"; // Menampilkan teks
+        text.style.display = "block";
+        button.textContent = "Sembunyikan Teks";
     } else {
-        text.style.display = "none"; // Menyembunyikan teks
+        text.style.display = "none"; 
+        button.textContent = "Tampilkan";
+        
     }
 }
 
 function togglekreatif() {
+    const button = document.getElementById("cn");
     var text = document.getElementById("KREATIF");
     if (text.style.display === "none") {
-        text.style.display = "block"; // Menampilkan teks
+        text.style.display = "block";
+        button.textContent = "Sembunyikan Teks";
     } else {
-        text.style.display = "none"; // Menyembunyikan teks
+        text.style.display = "none"; 
+        button.textContent = "Tampilkan";
+        
     }
 }
 
 function toggletransformatif() {
+    const button = document.getElementById("dn");
     var text = document.getElementById("TRANSFORMATIF");
     if (text.style.display === "none") {
-        text.style.display = "block"; // Menampilkan teks
+        text.style.display = "block";
+        button.textContent = "Sembunyikan Teks";
     } else {
-        text.style.display = "none"; // Menyembunyikan teks
+        text.style.display = "none"; 
+        button.textContent = "Tampilkan";
+        
     }
 }
 
 function toggleislami() {
+    const button = document.getElementById("en");
     var text = document.getElementById("ISLAMI");
     if (text.style.display === "none") {
-        text.style.display = "block"; // Menampilkan teks
+        text.style.display = "block";
+        button.textContent = "Sembunyikan Teks";
     } else {
-        text.style.display = "none"; // Menyembunyikan teks
+        text.style.display = "none"; 
+        button.textContent = "Tampilkan";
+        
     }
 }
